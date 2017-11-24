33868d0c78d7cf33ec5e179d7ed866fd9c477139
Patrick Williams <patrickrandallwilliams1992@gmail.com>
2017-11-23 19:19:26 -0500

    Update index.js

--- src/client/pages/Home/index.js
+++ src/client/pages/Home/index.js
@@ -44,7 +44,7 @@ class Home extends Component {
     }
 }
 
-const mapStateToProps = ({menu}) => menu;
+const mapStateToProps = ({menu}) => {return {menu}};
 
 const loadData = async (Model) => {
     let {MenuItem} = Model;
