de8459eb4e0ebe219bef56e52535b34773065b02
Pat Williams <patrick.rw92@gmail.com>
2017-12-10 14:03:26 -0500

    quick fix

---
+++ src/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: '326569157181-845i2pdap63vliqtt3pf06tel506cpgp.apps.googleusercontent.com',
+    googleClientSecret: 'mljxsvZ1W2zsthpeoEiGmCAF',
+    facebookClientID: '1940931679499816',
+    facebookClientSecret: 'abdc2af43b9516a37156f3d64a2e2110',
+    mongoURI: 'mongodb://restaurant-dev:rest-dev@ds117316.mlab.com:17316/restaurant-dev',
+    cookieKey: 'kljsndgkjlnsdkjnfqwttplskrndi'
+};
+
+//heroku config:set FACEBOOK_CLIENT_ID=1940931679499816
